84d1b10ea29483b0fb756049ed6f87160c32a3cb
Jirka Lewandowski <jirka.lewandowski@wzb.eu>
2015-07-24 19:34:31 +0200

    Fundamental API function and connection creator

---
+++ R/connect.R
@@ -0,0 +1,47 @@
+#' Connect to a specific gitlab instance API
+#' 
+#' Creates a function that can be used to issue request to the specified
+#' gitlab API instance with the specified user private token.
+#' 
+#' @details
+#' The returned function should serve as the primary way to access the gitlab
+#' API in the following. It can take vector/character arguments in the same way
+#' as the function \code{\link{gitlab}} does, as well as the convenience functions
+#' provided by this package or written by the user. If it is passed such that
+#' function it calls it with the arguments provided in \code{...} and the gitlab
+#' URL, api location and private_token provided when creating it via \code{gitlab_connection}.
+#' 
+#' @examples
+#' \dontrun{
+#' my_gitlab <- gitlab_connection("http://gitlab.example.com", "123####89")
+#' my_gitlab("projects")
+#' my_gitlab(get_file, "test-project", "README.md", ref = "dev")
+#' }
+#' 
+#' @param gitlab_url URL to the gitlab instance (e.g. \code{https://gitlab.myserver.com})
+#' @param private_token private_token with which to identify; must be created for a user on the gitlab web interface
+#' @param api_location location of the gitlab API under the \code{gitlab_url}, usually and by default "/api/v3/"
+#' 
+#' @return 
+#' 
+#' @export
+gitlab_connection <- function(gitlab_url
+                            , private_token
+                            , api_location = "/api/v3/") {
+  
+  gl_con_root <- paste0(gitlab_url, api_location)
+  
+  return(function(request, ...) {
+    if (is.function(request)) {
+      request(api_root = gl_con_root
+            , private_token = private_token
+            , ...)
+    } else {
+      gitlab(req = request
+           , api_root = gl_con_root
+           , private_token = private_token
+           , ...)
+    }
+  })
+}
+
---
+++ R/gitlab_api.R
@@ -0,0 +1,45 @@
+#' Request Gitlab API
+#' 
+#' @param req vector of characters that represents the call (e.g. \code{c("projects", project_id, "events")})
+#' @param api_root URL where the gitlab API to request resides (e.g. \code{https://gitlab.myserver.com/api/v3/})
+#' @param private_token to use for the API request
+#' @param verb http verb to use for request in form of one of the \code{httr} functions
+#' \code{\link[httr]{GET}}, \code{\link[httr]{PUT}}, \code{\link[httr]{POST}}, \code{\link[httr]{DELETE}}
+#' @param ... named parameters to pass on to gitlab API (technically: modifies query parameters of request URL)
+#' @export
+gitlab <- function(req
+                 , api_root
+                 , private_token
+                 , verb = httr::GET
+                 , ...) {
+  
+  req %>%
+    paste(collapse = "/") %>%
+    prefix(api_root, "/") %>%
+    verb(query = list(private_token = private_token, ...)) %>%
+    http_error_or_content() %>%
+    iff(is.nested.list, json_to_flat_df) ## better would be to check MIME type
+}
+
+http_error_or_content <- function(response
+                                , handle = httr::stop_for_status
+                                , ...) {
+  if (handle(response)) {
+    httr::content(response, ...)
+  }
+}
+
+is.nested.list <- function(l) {
+  is.list(l) && any(unlist(lapply(l, is.list)))
+}
+
+json_to_flat_df <- function(l) {
+  l %>%
+    lapply(unlist, recursive = TRUE) %>%
+    lapply(function(row) {  ## Is there a way to use Compose and Curry instead?
+      row %>%
+        lapply(unlist, use.names = FALSE) %>%
+        as.data.frame(stringsAsFactors = FALSE)
+    }) %>%
+    bind_rows()
+}
