1919afcb95aba4d453512b33c78082cd7d7426ed
Jirka Lewandowski <jirka.lewandowski@posteo.de>
2015-08-10 01:14:24 +0200

    Posting and editing of issues, using projectwide issue_ids

--- R/issues.R
+++ R/issues.R
@@ -1,14 +1,14 @@
 #' Get issues of a project or user
 #' 
 #' @param project project name or id, may be null for all issues created by user
-#' @param issue_id optional issue id (gitlab API id not iid)
+#' @param issue_id optional issue id (projectwide iid, not gitlab API id)
 #' @param ... further parameters passed on to \code{\link{gitlab}}, may be
 #' state, labels, issue id, ...
 #' @export
 get_issues <- function(project = NULL
                      , issue_id = NULL
                      , ...) {
-  (if (is.null(project)) "issues" else c("projects", to_project_id(project, ...), "issues", issue_id)) %>%
+  (if (is.null(project)) "issues" else c("projects", to_project_id(project, ...), "issues", to_issue_id(issue_id, project, ...))) %>%
     gitlab(auto_format = is.null(issue_id), ...) %>%
     iffn(is.null(issue_id), function(issue) {
       issue %>%
@@ -18,4 +18,64 @@ get_issues <- function(project = NULL
     })
 }
 
-## TODO post and edit issues
+to_issue_id <- function(issue_id, project, ...) {
+  if (is.null(issue_id)) {
+    NULL
+  } else {
+    get_issues(project = project, ...) %>%
+      filter(iid == issue_id) %>%
+      select(id) %>%
+      unlist() %>%
+      iff(function(x){length(x) == 0}, function(x) {
+        stop(paste("No issue with id", issue_id, "in project", project))})
+  }
+}
+
+#' Post a new issue or edit one
+#' 
+#' @param project project where the issue should be posted
+#' @param title title of the issue
+#' @param ... further parameters passed to the API call, may 
+#' contain description, asignee_id, milestone_id, labels, state_event (for edit_issue).
+#' 
+#' @rdname edit_issue
+#' @export
+new_issue <- function(project
+                    , title
+                    , ...) {
+  gitlab(c("projects", to_project_id(project, ...), "issues")
+      , title = title
+      , verb = httr::POST
+      , auto_format = FALSE
+      , ...)
+}
+
+#' Post a new issue or edit one
+#' 
+#' @param issue_id id of issue to edit  (projectwide iid, not gitlab API id)
+#' @export
+edit_issue <- function(project
+                    , issue_id
+                    , ...) {
+  gitlab(c("projects", to_project_id(project, ...), "issues", to_issue_id(issue_id, project, ...))
+         , verb = httr::PUT
+         , auto_format = FALSE
+         , ...)
+}
+
+#' @rdname edit_issue
+#' @export
+close_issue <- function(project 
+                      , issue_id
+                      , ...) {
+  edit_issue(project, issue_id, state_event = "close", ...)
+}
+
+#' @rdname edit_issue
+#' @export
+reopen_issue <- function(project 
+                       , issue_id
+                       , ...) {
+  edit_issue(project, issue_id, state_event = "reopen", ...)
+}
+
--- tests/testthat/test_issues.R
+++ tests/testthat/test_issues.R
@@ -9,6 +9,25 @@ test_that("getting issues works", {
   expect_is(my_gitlab(get_issues), "data.frame")
   expect_is(my_gitlab(get_issues, "testor"), "data.frame")
   expect_is(my_gitlab(get_issues, "testor", state = "closed"), "data.frame")
-  expect_is(my_gitlab(get_issues, "testor", 83), "data.frame")
+  expect_is(my_gitlab(get_issues, "testor", 2), "data.frame")
+  
+})
+
+test_that("editing issues works", {
+  
+  ## reopen issue 2
+  my_gitlab(reopen_issue, "testor", 2)
+  expect_true(my_gitlab(get_issues, "testor", 2)$state == "reopened")
+  
+  ## edit its description
+  my_gitlab(edit_issue, "testor", 2, description = "This is a test")
+  expect_true(my_gitlab(get_issues, "testor", 2)$description == "This is a test")
+  my_gitlab(edit_issue, "testor", 2, description = "This is not a test")
+  expect_false(my_gitlab(get_issues, "testor", 2)$description == "This is a test")
+  
+  ## close it
+  my_gitlab(close_issue, "testor", 2)
+  expect_true(my_gitlab(get_issues, "testor", 2)$state == "closed")
+  
   
 })
