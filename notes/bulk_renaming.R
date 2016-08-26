af4f18ff986f61e5fcdbbcdbff9cd928c881641a
Jirka Lewandowski <jirka.lewandowski@wzb.eu>
2016-08-26 15:58:02 +0200

    Simpler and more general renaming script

--- notes/bulk_renaming.R
+++ notes/bulk_renaming.R
@@ -13,36 +13,29 @@ data_frame(old_name = ls(envir = as.environment("package:gitlabr"))) %>%
     TRUE ~ paste0("gl_", .$old_name)
   )) %>%
   subset(old_name != new_name) %>%
-  mutate(parameters = old_name %>%
-           lapply(function(name) { formals(get(name, envir = as.environment("package:gitlabr"))) }) %>%
-           lapply(names),
-         head = old_name %>%
-           lapply(function(name) { formals(get(name, envir = as.environment("package:gitlabr"))) }) %>%
-           as.character() %>%
-           { gsub("pairlist(", "function(", ., fixed = TRUE) } %>%
-           { gsub("(\\s=\\s)(,|\\))", "\\2", .) },
-         body = old_name %>%
-           lapply(function(name) { formals(get(name, envir = as.environment("package:gitlabr"))) }) %>%
-           lapply(names) %>%
-           lapply(function(x) { gsub("(\\w+)", "\\1 = \\1", x) }) %>%
-           lapply(paste, collapse = ", ") %>%
-           lapply(function(args) { paste0("(", args, ")")}) %>%
-           unlist()) %>%
   mutate(complete = paste0("#' @export
 #' @rdname gitlabr_legacy
-", old_name, " <- ", head, " {
+", old_name, " <- function(...) {
   .Deprecated('", new_name, "', package = 'gitlabr')
-  ", new_name, body, "
-}")) -> replacements
+  ", new_name, "(...)
+}
+")) -> replacements
 
-replacements$parameters %>%
-  unlist() %>%
-  unique() %>%
-  setdiff("...")
+replacements %>%
+  mutate(doc_entry = paste0("#'    \\code{", old_name, "} \\tab is now called \\code{", new_name, "}")) %$%
+  { c("#' Legacy functions",
+      "#' @param ... Parameters to the new function",
+      "#' @name gitlabr_legacy",
+      "#' @section Details:",
+      "#' \\tabular{rl}{",
+      doc_entry,
+      "#' }",
+      "NULL",
+      "") } -> documentation_header
 
 unlink("R/legacy_headers.R")
 replacements %$%
-  paste(complete, collapse = "\n\n") %>%
+  paste(c(documentation_header, complete), collapse = "\n") %>%
   writeLines("R/legacy_headers.R")
 
 replacements %>% select(old_name, new_name) -> gitlabr_0_7_renaming
