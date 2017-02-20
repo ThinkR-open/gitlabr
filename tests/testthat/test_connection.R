e6190e93ff338364fef9e80e13f66a2be45d4982
Jirka Lewandowski <jirka.lewandowski@posteo.de>
2017-02-21 00:08:58 +0100

    bugfix

--- tests/testthat/test_connection.R
+++ tests/testthat/test_connection.R
@@ -1,7 +1,7 @@
 test_url <- Sys.getenv("GITLABR_TEST_URL")
 test_private_token <- Sys.getenv("GITLABR_TEST_TOKEN")
 test_login <- Sys.getenv("GITLABR_TEST_LOGIN")
-test_password <- Sys.geten("GITLABR_TEST_PASSWORD")
+test_password <- Sys.getenv("GITLABR_TEST_PASSWORD")
 
 test_that("Gitlab connection creation works", {
   
