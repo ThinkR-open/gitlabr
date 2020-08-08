7581c774465ef6bb9978f2e4b134f5188991b734
Sébastien Rochette <sebastienrochettefr@gmail.com>
2020-08-08 15:59:19 +0200

    No test if no token

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,16 +1,27 @@
 library(testthat)
 library(gitlabr)
 
-if (Sys.getenv("GITLABR_TEST_API_VERSION") == "") {
-  
-  Sys.setenv(GITLABR_TEST_API_VERSION = "v3")
-  test_check("gitlabr")
-  Sys.setenv(GITLABR_TEST_API_VERSION = "v4")
-  test_check("gitlabr")
-  Sys.setenv(GITLABR_TEST_API_VERSION = "")
-  
-} else {
-  
-  test_check("gitlabr")
+if (file.exists("environment.yml")) {
+  do.call(
+    Sys.setenv,
+    yaml::yaml.load_file("environment.yml")
+  )
+}
 
+if (Sys.getenv("GITLABR_TEST_TOKEN") != "") {
+  # Skip all tests if no token
+  
+  if (Sys.getenv("GITLABR_TEST_API_VERSION") == "") {
+    
+    Sys.setenv(GITLABR_TEST_API_VERSION = "v3")
+    test_check("gitlabr")
+    Sys.setenv(GITLABR_TEST_API_VERSION = "v4")
+    test_check("gitlabr")
+    Sys.setenv(GITLABR_TEST_API_VERSION = "")
+    
+  } else {
+    
+    test_check("gitlabr")
+    
+  }
 }
