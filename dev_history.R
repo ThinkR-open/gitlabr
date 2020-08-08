17e2f1dc85dd37ca455fd156898d3e6eba57c1f7
Sébastien Rochette <sebastienrochettefr@gmail.com>
2020-08-08 13:22:42 +0200

    tests and pkgdown

--- dev_history.R
+++ dev_history.R
@@ -5,6 +5,7 @@ usethis::use_git_ignore("tests/environment.yml")
 
 # Add CI
 usethis::use_github_action_check_standard()
+usethis::use_github_action("pkgdown")
 
 # Development
 attachment::att_amend_desc(extra.suggests = "R.rsp")
