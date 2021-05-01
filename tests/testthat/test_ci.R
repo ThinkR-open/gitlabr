4de40e1479af9c34c82a3d60eed56363add43e24
statnmap <sebastien@thinkr.fr>
2021-05-01 13:01:08 +0200

    Modify CI use and tests
    issue #21
    closes issue #13

--- tests/testthat/test_ci.R
+++ tests/testthat/test_ci.R
@@ -14,6 +14,7 @@ test_that("CI yml generation works", {
   # 
   expect_equal(yaml::yaml.load_file(ci_path),
                yaml::yaml.load_file("gitlab-ci.yml"))
+  expect_true(file.exists(file.path(dirname(ci_path), ".Rbuildignore")))
   
   on.exit(unlink(ci_path))
   
@@ -21,36 +22,18 @@ test_that("CI yml generation works", {
 
 
 test_that("CI builds access works", {
-  
-  my_gitlab <- gl_connection(test_url,
-                             private_token = test_private_token,
-                             api_version = test_api_version)
-  my_project <- gl_project_connection(test_url,
-                                      project = test_project,
-                                      private_token = test_private_token,
-                                      api_version = test_api_version)
-  
-    expect_is(my_gitlab(gl_jobs, project = test_project), "data.frame")
-    expect_is(my_project(gl_jobs), "data.frame")
-    expect_is(my_gitlab(gl_pipelines, project = test_project), "data.frame")
-    expect_is(my_project(gl_pipelines), "data.frame")
-    
-  if (test_api_version == 3) {
-    expect_is(my_gitlab(gl_builds, project = test_project, api_version = 3), "data.frame")
-    expect_is(my_project(gl_builds, api_version = 3), "data.frame")
-    expect_warning(my_project(gl_builds, api_version = 4), regexp = "deprecated")
-  }
-  
-  all_jobs <- my_gitlab(gl_jobs, test_project)
+
+  all_jobs <- gl_jobs(project = test_project)
   expect_is(all_jobs, "data.frame")
-  all_pipelines <- my_gitlab(gl_pipelines, test_project)
+  all_pipelines <- gl_pipelines(project = test_project)
   expect_is(all_pipelines, "data.frame")
   
   # issue #13
   # Create a job that will save an artifact
-  artifacts_zip <- my_gitlab(gl_latest_build_artifact, project = test_project, job = "testing")
+  artifacts_zip <- gl_latest_build_artifact(project = test_project, job = "testing")
   expect_true(file.exists(artifacts_zip))
   expect_true("test.txt" %in% unzip(artifacts_zip, list = TRUE)$Name)
   
 })
   
+# gl_ci_job("build", allowed_dependencies = "test")
