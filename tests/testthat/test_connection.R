a106324768b76d3d9203b5b1906cf130894f43e5
statnmap <sebastien@thinkr.fr>
2021-05-27 15:55:31 +0200

    Fix use of max_page and connection tests
    - fix issue #14

--- tests/testthat/test_connection.R
+++ tests/testthat/test_connection.R
@@ -1,77 +1,71 @@
 # Unset as it is set in helper.R
 unset_gitlab_connection()
 
-my_gitlab_test <- gl_connection(test_url,
-                           private_token = test_private_token,
-                           api_version = test_api_version)
+# gl_connection ----
+# Set GitLab connection for all tests
+my_gitlab_test <- gl_connection(
+  gitlab_url = test_url,
+  private_token = test_private_token,
+  api_version = test_api_version)
 
-# Only test if connection works ----
-
+# Note that we cannot compare directly all outputs because GitLab projects are actively increasing
 # Way 1
 my_gitlab_projects_output_raw <- my_gitlab_test("projects", max_page = 1)
-# Because download may be different as gitlab.com gets new projects modifications every second
-# We need to filter out a smaller set of projects to compare together
-first_ids <- my_gitlab_projects_output_raw$id[1:10]
-first_dates <- my_gitlab_projects_output_raw$created_at[1:10]
-# filter
-my_gitlab_projects_output <- my_gitlab_projects_output_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
 
 # Way_2
-my_gitlab_list_projects_output_raw <- my_gitlab(gl_list_projects, max_page = 1)
-# filter
-my_gitlab_list_projects_output <- my_gitlab_list_projects_output_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
+my_gitlab_list_projects_output_raw <- my_gitlab_test(gl_list_projects, max_page = 1)
 
 # Way_3
 gitlab_projects_api_raw <- gitlab("projects",
        api_root = paste0(test_url, "/api/v", test_api_version, "/"),
        private_token = test_private_token,
        max_page = 1)
-# filter
-gitlab_projects_api <- gitlab_projects_api_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
 
 # Way_4
-gl_list_projects_output_raw <- gl_list_projects(gitlab_con = my_gitlab, max_page = 1)
-# filter
-gl_list_projects_output <- gl_list_projects_output_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
+gl_list_projects_output_raw <- gl_list_projects(gitlab_con = my_gitlab_test, max_page = 1)
+
+# names with dots [.] only exist if there are sub-lists. 
+# This is not always the case depending on projects.
+# Names without dots are mandatory fields, apparently
+names_1 <- names(my_gitlab_projects_output_raw)[!grepl("[.]", names(my_gitlab_projects_output_raw))]
+names_2 <- names(my_gitlab_list_projects_output_raw)[!grepl("[.]", names(my_gitlab_list_projects_output_raw))]
+names_3 <- names(gitlab_projects_api_raw)[!grepl("[.]", names(gitlab_projects_api_raw))]
+names_4 <- names(gl_list_projects_output_raw)[!grepl("[.]", names(gl_list_projects_output_raw))]
 
 
 test_that("Gitlab connection creation works", {
 
-  expect_is(my_gitlab, "function")
+  expect_is(my_gitlab_test, "function")
   
-  expect_is(my_gitlab_projects_output, "data.frame")
-  expect_is(my_gitlab_list_projects_output, "data.frame")
+  expect_is(my_gitlab_projects_output_raw, "data.frame")
+  expect_is(my_gitlab_list_projects_output_raw, "data.frame")
+  expect_is(gitlab_projects_api_raw, "data.frame")
+  expect_is(gl_list_projects_output_raw, "data.frame")
   
-  # one page is 20
+  # one page is 20 lines
   expect_equal(nrow(my_gitlab_projects_output_raw), 20)
   expect_equal(nrow(my_gitlab_list_projects_output_raw), 20)
   expect_equal(nrow(gitlab_projects_api_raw), 20)
-  # issue #14
-  # Not working - See why max_page is not used in gl_list_projects_output?
-  # expect_equal(nrow(gl_list_projects_output_raw), 20)
-  # expect_equivalent(my_gitlab_list_projects_output,
-  #                   my_gitlab_projects_output)
+  expect_equal(nrow(gl_list_projects_output_raw), 20)
   
-  expect_equivalent(my_gitlab_projects_output,
-                    gitlab_projects_api)
-  
-  ## function idiom
-  expect_is(gl_list_projects_output, "data.frame")
-  
-  # issue #14
-  # Not working - See why max_page is not used in gl_list_projects_output?
-  # expect_equivalent(gl_list_projects_output,
-  #                   my_gitlab_projects_output)
-  # 
+  # Mandatory col names are all the same
+  expect_length(names_1[!names_1 %in% names_2], 0)
+  expect_length(names_1[!names_1 %in% names_3], 0)
+  expect_length(names_1[!names_1 %in% names_4], 0)
+  expect_length(names_2[!names_2 %in% names_1], 0)
+  expect_length(names_2[!names_2 %in% names_3], 0)
+  expect_length(names_3[!names_3 %in% names_4], 0)
+
 })
 
-my_project <- gl_project_connection(test_url, test_project,
-                                    private_token = test_private_token,
-                                    api_version = test_api_version)
+# gl_project_connection ----
+# Set project connection for all tests ----
+my_project <- gl_project_connection(
+  gitlab_url = test_url,
+  project = test_project,
+  private_token = test_private_token,
+  api_version = test_api_version)
+
 
 my_project_list_files <- my_project(gl_list_files, max_page = 1)
 my_gl_list_files <- gl_list_files(gitlab_con = my_project, max_page = 1)
@@ -88,60 +82,53 @@ test_that("Project connection creation works", {
 })
 
 # set_gitlab_connection ----
-# ## using explicit function creation
-# my_gitlab <- gl_connection(test_url,
-#                            private_token = test_private_token,
-#                            api_version = test_api_version)
-set_gitlab_connection(my_gitlab)
-# Way_1
+set_gitlab_connection(my_gitlab_test)
+# Note that we cannot compare directly all outputs because GitLab projects are actively increasing
+# Way_0 - gitlab_connection already set
 gitlab_projects_raw <- gitlab("projects", max_page = 1)
-# Because download may be different as gitlab.com gets new projects modifications every second
-# We need to filter out a smaller set of projects to compare together
-first_ids <- gitlab_projects_raw$id[1:10]
-first_dates <- gitlab_projects_raw$created_at[1:10]
-# filter
-gitlab_projects <- gitlab_projects_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
 
-# Way_2
-gitlab_projects_self_raw <- my_gitlab("projects", gitlab_con = "self", max_page = 1)
-# filter
-gitlab_projects_self <- gitlab_projects_self_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
+# Way 1 - gitlab_connection already set
+my_gitlab_projects_self_raw <- my_gitlab_test("projects", gitlab_con = "self", max_page = 1)
 
-# Way_3
-gitlab_list_projects_self_raw <- my_gitlab(gl_list_projects, gitlab_con = "self", max_page = 1)
-# filter
-gitlab_list_projects_self <- gitlab_list_projects_self_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
+# Way_2 - gitlab_connection already set
+my_gitlab_list_projects_self_raw <- my_gitlab_test(gl_list_projects, gitlab_con = "self", max_page = 1)
 
-# Way_4
+# Way_4 - gitlab_connection already set
 gl_list_projects_empty_raw <- gl_list_projects(max_page = 1)
-# filter
-gl_list_projects_empty <- gl_list_projects_empty_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
+
+# names with dots [.] only exist if there are sub-lists. 
+# This is not always the case depending on projects.
+# Names without dots are mandatory fields, apparently
+names_0 <- names(gitlab_projects_raw)[!grepl("[.]", names(gitlab_projects_raw))]
+names_1 <- names(my_gitlab_projects_self_raw)[!grepl("[.]", names(my_gitlab_projects_self_raw))]
+names_2 <- names(my_gitlab_list_projects_self_raw)[!grepl("[.]", names(my_gitlab_list_projects_self_raw))]
+names_4 <- names(gl_list_projects_empty_raw)[!grepl("[.]", names(gl_list_projects_empty_raw))]
 
 
 test_that("set_gl_connection works", {
+
+  expect_is(gitlab, "function")
   
-  # one page is 20
-  # Not working - See why max_page is not used ?
-  # expect_equal(nrow(gitlab_projects_raw), 20)
-  expect_equal(nrow(gitlab_projects_self_raw), 20)
-  expect_equal(nrow(gitlab_list_projects_self_raw), 20)
-  # issue #14
-  # Not working - See why max_page is not used ?
-  # expect_equal(nrow(gl_list_projects_empty_raw), 20)
+  expect_is(gitlab_projects_raw, "data.frame")
+  expect_is(my_gitlab_projects_self_raw, "data.frame")
+  expect_is(my_gitlab_list_projects_self_raw, "data.frame")
+  expect_is(gl_list_projects_empty_raw, "data.frame")
   
-  # issue #14
-  # Find why gitlab_projects_self and gitlab_list_projects_self have not the same number of columns than others
-  # expect_is(gitlab_projects, "data.frame")
-  # expect_equivalent(gitlab_projects,
-  #                   gitlab_projects_self)
-  # expect_equivalent(gl_list_projects_empty,
-  #                   gitlab_list_projects_self)
+  # one page is 20 lines
+  expect_equal(nrow(gitlab_projects_raw), 20)
+  expect_equal(nrow(my_gitlab_projects_self_raw), 20)
+  expect_equal(nrow(my_gitlab_list_projects_self_raw), 20)
+  expect_equal(nrow(gl_list_projects_empty_raw), 20)
   
+  # Mandatory col names are all the same
+  expect_length(names_1[!names_1 %in% names_2], 0)
+  expect_length(names_1[!names_1 %in% names_3], 0)
+  expect_length(names_1[!names_1 %in% names_0], 0)
+  expect_length(names_2[!names_2 %in% names_1], 0)
+  expect_length(names_2[!names_2 %in% names_3], 0)
+  expect_length(names_3[!names_3 %in% names_0], 0)
 })
+# unset connection
 unset_gitlab_connection()
 
 # set_gitlab_connection with dots ----
@@ -150,54 +137,53 @@ set_gitlab_connection(gitlab_url = test_url,
                       private_token = test_private_token,
                       api_version = test_api_version)
 
-# Way_1
+# Note that we cannot compare directly all outputs because GitLab projects are actively increasing
+# Way_0 - gitlab_connection already set
 gitlab_projects_raw <- gitlab("projects", max_page = 1)
-# Because download may be different as gitlab.com gets new projects modifications every second
-# We need to filter out a smaller set of projects to compare together
-first_ids <- gitlab_projects_raw$id[1:10]
-first_dates <- gitlab_projects_raw$created_at[1:10]
-# filter
-gitlab_projects <- gitlab_projects_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
 
-# Way_2
-gitlab_projects_self_raw <- my_gitlab("projects", gitlab_con = "self", max_page = 1)
-# filter
-gitlab_projects_self <- gitlab_projects_self_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
+# Way 1 - gitlab_connection already set
+my_gitlab_projects_self_raw <- my_gitlab_test("projects", gitlab_con = "self", max_page = 1)
 
-# Way_3
-gitlab_list_projects_self_raw <- my_gitlab(gl_list_projects, gitlab_con = "self", max_page = 1)
-# filter
-gitlab_list_projects_self <- gitlab_list_projects_self_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
+# Way_2 - gitlab_connection already set
+my_gitlab_list_projects_self_raw <- my_gitlab_test(gl_list_projects, gitlab_con = "self", max_page = 1)
 
-# Way_4
+# Way_4 - gitlab_connection already set
 gl_list_projects_empty_raw <- gl_list_projects(max_page = 1)
-# filter
-gl_list_projects_empty <- gl_list_projects_empty_raw %>% 
-  filter(id %in% first_ids,  created_at %in% first_dates)
+
+# names with dots [.] only exist if there are sub-lists. 
+# This is not always the case depending on projects.
+# Names without dots are mandatory fields, apparently
+names_0 <- names(gitlab_projects_raw)[!grepl("[.]", names(gitlab_projects_raw))]
+names_1 <- names(my_gitlab_projects_self_raw)[!grepl("[.]", names(my_gitlab_projects_self_raw))]
+names_2 <- names(my_gitlab_list_projects_self_raw)[!grepl("[.]", names(my_gitlab_list_projects_self_raw))]
+names_4 <- names(gl_list_projects_empty_raw)[!grepl("[.]", names(gl_list_projects_empty_raw))]
 
 
 test_that("set_gl_connection with dots works", {
 
-  # one page is 20
-  # Not working - See why max_page is not used ?
-  # expect_equal(nrow(gitlab_projects_raw), 20)
-  expect_equal(nrow(gitlab_projects_self_raw), 20)
-  expect_equal(nrow(gitlab_list_projects_self_raw), 20)
-  # issue #14
-  # Not working - See why max_page is not used ?
-  # expect_equal(nrow(gl_list_projects_empty_raw), 20)
+  expect_is(gitlab, "function")
   
-  expect_is(gitlab_projects, "data.frame")
+  expect_is(gitlab_projects_raw, "data.frame")
+  expect_is(my_gitlab_projects_self_raw, "data.frame")
+  expect_is(my_gitlab_list_projects_self_raw, "data.frame")
+  expect_is(gl_list_projects_empty_raw, "data.frame")
   
-  # issue #14
-  # Find why gitlab_projects_self and gitlab_list_projects_self have not the same number of columns than others
-  # expect_equivalent(gitlab_projects,
-  #                   gitlab_projects_self)
-  # expect_equivalent(gl_list_projects_empty,
-  #                   gitlab_list_projects_self)
+  # one page is 20 lines
+  expect_equal(nrow(gitlab_projects_raw), 20)
+  expect_equal(nrow(my_gitlab_projects_self_raw), 20)
+  expect_equal(nrow(my_gitlab_list_projects_self_raw), 20)
+  expect_equal(nrow(gl_list_projects_empty_raw), 20)
+  
+  # Mandatory col names are all the same
+  expect_length(names_1[!names_1 %in% names_2], 0)
+  expect_length(names_1[!names_1 %in% names_3], 0)
+  expect_length(names_1[!names_1 %in% names_0], 0)
+  expect_length(names_2[!names_2 %in% names_1], 0)
+  expect_length(names_2[!names_2 %in% names_3], 0)
+  expect_length(names_3[!names_3 %in% names_0], 0)
   
 })
 unset_gitlab_connection()
+
+# Set back the connection for the session as in helper.R
+set_gitlab_connection(my_gitlab)
