3f1277fe92a9d6868180f59975fd6e16627f3cbf
statnmap <sebastien@thinkr.fr>
2021-08-09 12:37:27 +0200

    test connection on users projects

--- tests/testthat/test_connection_env.R
+++ tests/testthat/test_connection_env.R
@@ -22,39 +22,48 @@ my_gitlab_test <- gl_connection(
   api_version = test_api_version)
 
 # Note that we cannot compare directly all outputs because GitLab projects are actively increasing
+# Instead, we will only check for project owned by the user with `owned = TRUE`
+
+# All projects should be the same (if user does not add a project during CI)
+# Except for last_activity_at that can change during and because of CI
+
 # Way 1
-my_gitlab_projects_output_raw <- my_gitlab_test("projects", max_page = 1)
+my_gitlab_projects_output_raw <- my_gitlab_test("projects", max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way_2
-my_gitlab_list_projects_output_raw <- my_gitlab_test(gl_list_projects, max_page = 1)
+my_gitlab_list_projects_output_raw <- my_gitlab_test(gl_list_projects, max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way_3
 gitlab_projects_api_raw <- gitlab("projects",
        api_root = paste0(test_url, "/api/v", test_api_version, "/"),
        private_token = test_private_token,
-       max_page = 1)
+       max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way_4
-gl_list_projects_output_raw <- gl_list_projects(gitlab_con = my_gitlab_test, max_page = 1)
+gl_list_projects_output_raw <- gl_list_projects(gitlab_con = my_gitlab_test, max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # names with dots [.] only exist if there are sub-lists. 
 # This is not always the case depending on projects.
 # Names without dots are also not always existing fields, apparently
-names_1 <- names(my_gitlab_projects_output_raw)[!grepl("[.]", names(my_gitlab_projects_output_raw))]
-names_2 <- names(my_gitlab_list_projects_output_raw)[!grepl("[.]", names(my_gitlab_list_projects_output_raw))]
-names_3 <- names(gitlab_projects_api_raw)[!grepl("[.]", names(gitlab_projects_api_raw))]
-names_4 <- names(gl_list_projects_output_raw)[!grepl("[.]", names(gl_list_projects_output_raw))]
+names_1 <- names(my_gitlab_projects_output_raw)#[!grepl("[.]", names(my_gitlab_projects_output_raw))]
+names_2 <- names(my_gitlab_list_projects_output_raw)#[!grepl("[.]", names(my_gitlab_list_projects_output_raw))]
+names_3 <- names(gitlab_projects_api_raw)#[!grepl("[.]", names(gitlab_projects_api_raw))]
+names_4 <- names(gl_list_projects_output_raw)#[!grepl("[.]", names(gl_list_projects_output_raw))]
 
 all_same <- function(.x) (isTRUE(all(is.na(.x)) || all(.x == .x[1])))
-all_same(1:4)
-all_same(c(NA, 1, 2, 3))
-all_same(c(NA, "", "", "a"))
-all_same(rep(1, 4))
-all_same(rep(NA, 4))
-all_same(rep("toto", 4))
-all_same(c("toto", "a"))
-all_same(rep(TRUE, 4))
-all_same(rep(FALSE, 4))
+# all_same(1:4)
+# all_same(c(NA, 1, 2, 3))
+# all_same(c(NA, "", "", "a"))
+# all_same(rep(1, 4))
+# all_same(rep(NA, 4))
+# all_same(rep("toto", 4))
+# all_same(c("toto", "a"))
+# all_same(rep(TRUE, 4))
+# all_same(rep(FALSE, 4))
 
 # Retrieve all projects in common to be sure all infos are there, and missing one are empty
 # Get id present in all four ways
@@ -84,12 +93,12 @@ test_that("GitLab connection creation works", {
   expect_is(gitlab_projects_api_raw, "data.frame")
   expect_is(gl_list_projects_output_raw, "data.frame")
   
-  # one page is 20 lines
-  expect_equal(nrow(my_gitlab_projects_output_raw), 20)
-  expect_equal(nrow(my_gitlab_list_projects_output_raw), 20)
-  expect_equal(nrow(gitlab_projects_api_raw), 20)
-  expect_equal(nrow(gl_list_projects_output_raw), 20)
-  
+  # one page is 20 lines max
+  expect_lte(nrow(my_gitlab_projects_output_raw), 20)
+  expect_lte(nrow(my_gitlab_list_projects_output_raw), 20)
+  expect_lte(nrow(gitlab_projects_api_raw), 20)
+  expect_lte(nrow(gl_list_projects_output_raw), 20)
+
   # Col names in common should be greater than zero
   expect_gt(length(names_1[names_1 %in% names_2]), 0)
   expect_gt(length(names_1[names_1 %in% names_3]), 0)
@@ -98,11 +107,33 @@ test_that("GitLab connection creation works", {
   expect_gt(length(names_2[names_2 %in% names_3]), 0)
   expect_gt(length(names_3[names_3 %in% names_4]), 0)
 
+  # We keep only user projects
+  # All projects should be the same (if user does not add a project during CI)
+  # Except for last_activity_at that can change during and because of CI
+  expect_equal(my_gitlab_projects_output_raw, my_gitlab_list_projects_output_raw)
+  expect_equal(my_gitlab_projects_output_raw, gitlab_projects_api_raw)
+  expect_equal(my_gitlab_projects_output_raw, gl_list_projects_output_raw)
+  
   # All values are the same (everything should be TRUE)
   if (length(names(id_common[id_common == FALSE]) != 0)) {
-    warning("Names with not common info: ", names(id_common[id_common == FALSE]))
+    warning("Names with not common info: ", 
+            paste(names(id_common[id_common == FALSE]), collapse = ", "))
   }
   expect_equal(names(id_common[id_common == FALSE]), character(0))
+
+})
+
+# Test list ok even if not owned ----
+my_gitlab_projects_owned <- my_gitlab_test("projects", max_page = 1, owned = TRUE)
+my_gitlab_projects_all_public <- my_gitlab_test("projects", max_page = 1)
+
+test_that("Access to all public repo works", {
+  expect_lte(nrow(my_gitlab_projects_owned), 20)
+  expect_equal(nrow(my_gitlab_projects_all_public), 20)
+  
+  # There may be one in common because CI updates users' projects
+  # There should be at least one difference fi user does not create 20 repo at the same time
+  expect_true(any(my_gitlab_projects_owned[["id"]] != my_gitlab_projects_all_public[["id"]]))
 })
 
 # gl_project_connection ----
@@ -132,24 +163,28 @@ test_that("Project connection creation works", {
 set_gitlab_connection(my_gitlab_test)
 # Note that we cannot compare directly all outputs because GitLab projects are actively increasing
 # Way_0 - gitlab_connection already set
-gitlab_projects_raw <- gitlab("projects", max_page = 1)
+gitlab_projects_raw <- gitlab("projects", max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way 1 - gitlab_connection already set
-my_gitlab_projects_self_raw <- my_gitlab_test("projects", gitlab_con = "self", max_page = 1)
+my_gitlab_projects_self_raw <- my_gitlab_test("projects", gitlab_con = "self", max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way_2 - gitlab_connection already set
-my_gitlab_list_projects_self_raw <- my_gitlab_test(gl_list_projects, gitlab_con = "self", max_page = 1)
+my_gitlab_list_projects_self_raw <- my_gitlab_test(gl_list_projects, gitlab_con = "self", max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way_4 - gitlab_connection already set
-gl_list_projects_empty_raw <- gl_list_projects(max_page = 1)
+gl_list_projects_empty_raw <- gl_list_projects(max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # names with dots [.] only exist if there are sub-lists. 
 # This is not always the case depending on projects.
 # Names without dots are mandatory fields, apparently
-names_0 <- names(gitlab_projects_raw)[!grepl("[.]", names(gitlab_projects_raw))]
-names_1 <- names(my_gitlab_projects_self_raw)[!grepl("[.]", names(my_gitlab_projects_self_raw))]
-names_2 <- names(my_gitlab_list_projects_self_raw)[!grepl("[.]", names(my_gitlab_list_projects_self_raw))]
-names_4 <- names(gl_list_projects_empty_raw)[!grepl("[.]", names(gl_list_projects_empty_raw))]
+names_0 <- names(gitlab_projects_raw)#[!grepl("[.]", names(gitlab_projects_raw))]
+names_1 <- names(my_gitlab_projects_self_raw)#[!grepl("[.]", names(my_gitlab_projects_self_raw))]
+names_2 <- names(my_gitlab_list_projects_self_raw)#[!grepl("[.]", names(my_gitlab_list_projects_self_raw))]
+names_4 <- names(gl_list_projects_empty_raw)#[!grepl("[.]", names(gl_list_projects_empty_raw))]
 
 # Retrieve all projects in common to be sure all infos are there, and missing one are empty
 # Get id present in all four ways
@@ -180,11 +215,11 @@ test_that("set_gl_connection works", {
   expect_is(my_gitlab_list_projects_self_raw, "data.frame")
   expect_is(gl_list_projects_empty_raw, "data.frame")
   
-  # one page is 20 lines
-  expect_equal(nrow(gitlab_projects_raw), 20)
-  expect_equal(nrow(my_gitlab_projects_self_raw), 20)
-  expect_equal(nrow(my_gitlab_list_projects_self_raw), 20)
-  expect_equal(nrow(gl_list_projects_empty_raw), 20)
+  # one page is 20 lines max
+  expect_lte(nrow(gitlab_projects_raw), 20)
+  expect_lte(nrow(my_gitlab_projects_self_raw), 20)
+  expect_lte(nrow(my_gitlab_list_projects_self_raw), 20)
+  expect_lte(nrow(gl_list_projects_empty_raw), 20)
   
   # Col names in common should be greater than zero
   expect_gt(length(names_1[names_1 %in% names_2]), 0)
@@ -194,9 +229,17 @@ test_that("set_gl_connection works", {
   expect_gt(length(names_2[names_2 %in% names_4]), 0)
   expect_gt(length(names_4[names_4 %in% names_0]), 0)
   
+  # We keep only user projects
+  # All projects should be the same (if user does not add a project during CI)
+  # Except for last_activity_at that can change during and because of CI
+  expect_equal(gitlab_projects_raw, my_gitlab_projects_self_raw)
+  expect_equal(gitlab_projects_raw, my_gitlab_list_projects_self_raw)
+  expect_equal(gitlab_projects_raw, gl_list_projects_empty_raw)
+  
   # All values are the same (everything should be TRUE)
   if (length(names(id_common[id_common == FALSE]) != 0)) {
-    warning("Names with not common info: ", names(id_common[id_common == FALSE]))
+    warning("Names with not common info: ", 
+            paste(names(id_common[id_common == FALSE]), collapse = ", "))
   }
   expect_equal(names(id_common[id_common == FALSE]), character(0))
 })
@@ -211,24 +254,28 @@ set_gitlab_connection(gitlab_url = test_url,
 
 # Note that we cannot compare directly all outputs because GitLab projects are actively increasing
 # Way_0 - gitlab_connection already set
-gitlab_projects_raw <- gitlab("projects", max_page = 1)
+gitlab_projects_raw <- gitlab("projects", max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way 1 - gitlab_connection already set
-my_gitlab_projects_self_raw <- my_gitlab_test("projects", gitlab_con = "self", max_page = 1)
+my_gitlab_projects_self_raw <- my_gitlab_test("projects", gitlab_con = "self", max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way_2 - gitlab_connection already set
-my_gitlab_list_projects_self_raw <- my_gitlab_test(gl_list_projects, gitlab_con = "self", max_page = 1)
+my_gitlab_list_projects_self_raw <- my_gitlab_test(gl_list_projects, gitlab_con = "self", max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # Way_4 - gitlab_connection already set
-gl_list_projects_empty_raw <- gl_list_projects(max_page = 1)
+gl_list_projects_empty_raw <- gl_list_projects(max_page = 1, owned = TRUE) %>% 
+  select(-last_activity_at)
 
 # names with dots [.] only exist if there are sub-lists. 
 # This is not always the case depending on projects.
 # Names without dots are mandatory fields, apparently
-names_0 <- names(gitlab_projects_raw)[!grepl("[.]", names(gitlab_projects_raw))]
-names_1 <- names(my_gitlab_projects_self_raw)[!grepl("[.]", names(my_gitlab_projects_self_raw))]
-names_2 <- names(my_gitlab_list_projects_self_raw)[!grepl("[.]", names(my_gitlab_list_projects_self_raw))]
-names_4 <- names(gl_list_projects_empty_raw)[!grepl("[.]", names(gl_list_projects_empty_raw))]
+names_0 <- names(gitlab_projects_raw)#[!grepl("[.]", names(gitlab_projects_raw))]
+names_1 <- names(my_gitlab_projects_self_raw)#[!grepl("[.]", names(my_gitlab_projects_self_raw))]
+names_2 <- names(my_gitlab_list_projects_self_raw)#[!grepl("[.]", names(my_gitlab_list_projects_self_raw))]
+names_4 <- names(gl_list_projects_empty_raw)#[!grepl("[.]", names(gl_list_projects_empty_raw))]
 
 # Retrieve all projects in common to be sure all infos are there, and missing one are empty
 # Get id present in all four ways
@@ -259,11 +306,10 @@ test_that("set_gl_connection with dots works", {
   expect_is(gl_list_projects_empty_raw, "data.frame")
   
   # one page is 20 lines
-  expect_equal(nrow(gitlab_projects_raw), 20)
-  expect_equal(nrow(my_gitlab_projects_self_raw), 20)
-  expect_equal(nrow(my_gitlab_list_projects_self_raw), 20)
-  expect_equal(nrow(gl_list_projects_empty_raw), 20)
-  
+  expect_lte(nrow(gitlab_projects_raw), 20)
+  expect_lte(nrow(my_gitlab_projects_self_raw), 20)
+  expect_lte(nrow(my_gitlab_list_projects_self_raw), 20)
+  expect_lte(nrow(gl_list_projects_empty_raw), 20)
   
   # Col names in common should be greater than zero
   expect_gt(length(names_1[names_1 %in% names_2]), 0)
@@ -273,10 +319,18 @@ test_that("set_gl_connection with dots works", {
   expect_gt(length(names_2[names_2 %in% names_4]), 0)
   expect_gt(length(names_4[names_4 %in% names_0]), 0)
   
+  # We keep only user projects
+  # All projects should be the same (if user does not add a project during CI)
+  # Except for last_activity_at that can change during and because of CI
+  expect_equal(gitlab_projects_raw, my_gitlab_projects_self_raw)
+  expect_equal(gitlab_projects_raw, my_gitlab_list_projects_self_raw)
+  expect_equal(gitlab_projects_raw, gl_list_projects_empty_raw)
+  
   # All values are the same (everything should be TRUE)
   # expect_equal(which(!id_common[1,]), 0)
   if (length(names(id_common[id_common == FALSE]) != 0)) {
-    warning("Names with not common info: ", names(id_common[id_common == FALSE]))
+    warning("Names with not common info: ", 
+            paste(names(id_common[id_common == FALSE]), collapse = ", "))
   }
   expect_equal(names(id_common[id_common == FALSE]), character(0))
 
@@ -288,3 +342,4 @@ set_gitlab_connection(
   gitlab_url = test_url,
   private_token = test_private_token,
   api_version = test_api_version)
+
